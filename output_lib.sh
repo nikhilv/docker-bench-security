efaf3f47af203cbfe6334d8586ce24be8552533d
Nikhil Vaze <nvaze@electric-cloud.com>
2015-06-20 23:18:14 -0700

    Remove Color

--- output_lib.sh
+++ output_lib.sh
@@ -10,17 +10,17 @@ logit () {
 }
 
 info () {
-  printf "%b\n" "${bldblu}[INFO]${txtrst} $1" | tee -a "$logger"
+  printf "%b\n" "[INFO] $1" | tee -a "$logger"
 }
 
 pass () {
-  printf "%b\n" "${bldgrn}[PASS]${txtrst} $1" | tee -a "$logger"
+  printf "%b\n" "[PASS] $1" | tee -a "$logger"
 }
 
 warn () {
-  printf "%b\n" "${bldred}[WARN]${txtrst} $1" | tee -a "$logger"
+  printf "%b\n" "[WARN] $1" | tee -a "$logger"
 }
 
 yell () {
-  printf "%b\n" "${bldylw}$1${txtrst}\n"
+  printf "%b\n" "$1\n"
 }
